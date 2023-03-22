b51ad8860ea9982169a47a31fbfce424a272b966
GioKatamadze <giokat.inbox@gmail.com>
2023-03-22 21:30:56 +0400

    connect postgresql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+import { TicketTierModule } from './ticket-tier/ticket-tier.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: `containers-us-west-148.railway.app`,
+      port: 8016,
+      username: 'postgres',
+      password: 'Qk2Rts91x9WES5au3sFA',
+      database: 'railway',
+      entities: [], // TODO: add your entities here
+      synchronize: true, // set to false in production
+    }),
+    TicketTierModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private readonly connection: Connection) {
+    console.log('Database connection established successfully');
+  }
+}
